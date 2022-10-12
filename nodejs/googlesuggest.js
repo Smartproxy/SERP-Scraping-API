ee62c0aca3f41baa069be6424fbae6e75d297470
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-10-12 13:37:29 +0300

    Add files via upload

--- nodejs/googlesuggest.js
+++ nodejs/googlesuggest.js
@@ -1,11 +1,6 @@
 const sdk = require('api')('@smartproxy/v1.0#25e7913l1ow524w');
 
-sdk.auth('SPusername', 'SPassword');
-sdk.realTimeExample({
-  target: 'google_images',
-  query: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg',
-  parse: false,
-  geo: 'London,England,United Kingdom'
-})
+sdk.auth('SPusername', 'SPpassword');
+sdk.realTimeExample({target: 'google_suggest', query: 'world', parse: false})
   .then(res => console.log(res))
   .catch(err => console.error(err));
