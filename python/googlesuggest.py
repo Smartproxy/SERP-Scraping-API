08b25fe72320c3651009f4850298e2e75eb726bb
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-10-12 13:38:31 +0300

    Add files via upload

--- python/googlesuggest.py
+++ python/googlesuggest.py
@@ -1,19 +1,18 @@
 import requests
 
-url = "https://scrape.smartproxy.com/v1/tasks"
-
-payload = {
-    "target": "google_images",
-    "query": "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg",
-    "parse": False,
-    "geo": "London,England,United Kingdom"
-}
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": "Basic U1B1c2VybmFtZTpTUGFzc3dvcmQ="
+task_params = {
+    'target': 'google_suggest',
+    'query': 'world',
+    'parse': False
 }
 
-response = requests.post(url, json=payload, headers=headers)
+username = 'SPusername'
+password = 'SPpassword'
+
+response = requests.post(
+    'https://scrape.smartproxy.com/v1/tasks',
+    json = task_params,
+    auth = (username, password)
+)
 
 print(response.text)
