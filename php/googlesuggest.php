8c1a8217733a087f470c9a8a354dbf8e60069dcd
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-10-12 13:38:08 +0300

    Add files via upload

--- php/googlesuggest.php
+++ php/googlesuggest.php
@@ -4,11 +4,11 @@ require_once('vendor/autoload.php');
 $client = new \GuzzleHttp\Client();
 
 $response = $client->request('POST', 'https://scrape.smartproxy.com/v1/tasks', [
-  'body' => '{"target":"google_images","query":"https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg","parse":false,"geo":"London,England,United Kingdom"}',
+  'body' => '{"target":"google_suggest","query":"world","parse":false}',
   'headers' => [
-    'Accept' => 'application/json',
-    'Authorization' => 'Basic U1B1c2VybmFtZTpTUGFzc3dvcmQ=',
     'Content-Type' => 'application/json',
+    'accept' => 'application/json',
+    'authorization' => 'Basic U1B1c2VybmFtZTpTUHBhc3N3b3Jk',
   ],
 ]);
 
