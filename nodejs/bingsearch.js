38fc8af1ae1a0b7d8a3b851df6e8a75e87360b5b
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-12-21 14:46:30 +0200

    Add files via upload

--- nodejs/bingsearch.js
+++ nodejs/bingsearch.js
@@ -1,12 +1,24 @@
-curl --request POST \
-     --url https://scrape.smartproxy.com/v1/tasks \
-     --header 'Accept: application/json' \
-     --header 'Authorization: Basic U1B1c2VybmFtZTpTUHBhc3N3b3Jk' \
-     --header 'Content-Type: application/json' \
-     --data '
-{
-     "target": "bing",
-     "parse": true,
-     "url": "https://www.bing.com/search?q=history"
-}
-'
+const request = require('request');
+
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+
+const options = {
+  method: 'POST',
+  url: 'https://scrape.smartproxy.com/v1/tasks',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
+  },
+  body: JSON.stringify({
+    target: 'bing',
+    parse: true,
+    url: 'https://www.bing.com/search?q=history'
+  })
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
