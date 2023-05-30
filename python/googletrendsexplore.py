e35b4485d8cb315dfb84b4c137a706d23f8d54f9
Noah <45659684+NoahDrucker@users.noreply.github.com>
2023-05-30 13:21:05 +0300

    Add files via upload

---
+++ python/googletrendsexplore.py
@@ -0,0 +1,24 @@
+import requests
+
+task_params = {
+
+    'target':'google_trends_explore',
+    'query': 'pizza',
+    'search_type':'web_search',
+    'date_start': '2022-01-10',
+    'date_end': '2023-01-10',
+    'geo':'US',
+    'locale':'en-us',
+    'device_type':'desktop'
+}
+
+username = 'SPusername'
+password = 'SPpassword'
+
+response = requests.post(
+    'https://scrape.smartproxy.com/v1/tasks',
+    json = task_params,
+    auth = (username, password)
+)
+
+print(response.text)
