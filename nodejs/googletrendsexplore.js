a0de7d05a61e1f0191d8240289d4d3c1bab6bce1
Noah <45659684+NoahDrucker@users.noreply.github.com>
2023-05-30 13:20:24 +0300

    Add files via upload

---
+++ nodejs/googletrendsexplore.js
@@ -0,0 +1,21 @@
+const response = await fetch(
+    'https://scrape.smartproxy.com/v1/tasks', {
+        method: 'POST',
+        body: {
+                target:'google_trends_explore',
+                query: 'pizza',
+                search_type:'web_search',
+                date_start: '2022-01-10',
+                date_end: '2023-01-10',
+                geo:'US',
+                locale:'en-us',
+                device_type:'desktop'
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic AUTH'
+        },
+    }
+).catch(error => console.log(error));
+
+console.log(response)
